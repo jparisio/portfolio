f6f5b8069665f87b0d0ed4f30b2c74ba0f372ffd
Justin Parisio <justinparisio@justins-m3-mbp.home>
2024-09-14 01:50:35 -0400

    stuff

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -14,14 +14,23 @@ export default function Projects() {
 
   const scale = useTransform(
     scrollYProgress,
-    [0, 0.5, 1],
-    ["100%", "50%", "100%"]
+    [0, 0.4, 1],
+    ["75%", "100%", "75%"]
   );
 
+  const x = useTransform(scrollYProgress, [0.2, 1], [-200, 200]);
+  const opacity = useTransform(
+    scrollYProgress,
+    [0, 0.3, 0.4, 1],
+    ["0%", "0%", "100%", "100%"]
+  );
   return (
     <div className="projects-container" ref={container}>
       <div className="inner">
-        <motion.h1 style={{ scale }}>Projects</motion.h1>
+        <motion.h1 style={{ scale }}>⌄</motion.h1>
+      </div>
+      <div className="inner-1">
+        <motion.h1 style={{ x, opacity }}>1</motion.h1>
       </div>
       <div className="inner2"></div>
     </div>
