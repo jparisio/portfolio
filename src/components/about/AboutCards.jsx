b025db7419d2e9c4be3c288ddb8961b310785a19
Justin Parisio <justinparisio@justins-m3-mbp.home>
2024-09-13 20:20:28 -0400

    i actually cant figure this out dude

--- src/components/about/AboutCards.jsx
+++ src/components/about/AboutCards.jsx
@@ -1,20 +1,68 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import { motion } from "framer-motion";
-import "./AboutCards.css";
+import "./AboutCards.css"; // Import your CSS file
 
-export default function AboutCards({ key, item, index, isInView }) {
+export default function AboutCards({ item, index, isInView }) {
+  const [mousePos, setMousePos] = useState({ x: 0, y: 0 });
+  const [circleSize, setCircleSize] = useState(0);
+  const cardRef = useRef(null);
+
+  const handleMouseEnter = (e) => {
+    const { left, top, width, height } =
+      cardRef.current.getBoundingClientRect();
+    const x = ((e.clientX - left) / width) * 100;
+    const y = ((e.clientY - top) / height) * 100;
+
+    setMousePos({ x, y });
+    setCircleSize(15); // Grow the circle to its full size
+
+    // Log the calculated relative mouse position
+    console.log("Mouse Enter Position:", {
+      x: e.clientX - left,
+      y: e.clientY - top,
+    });
+  };
+
+  const handleMouseLeave = (e) => {
+    const { left, top, width, height } =
+      cardRef.current.getBoundingClientRect();
+    const x = ((e.clientX - left) / width) * 100;
+    const y = ((e.clientY - top) / height) * 100;
+
+    setMousePos({ x, y });
+    setCircleSize(0);
+
+    // Log the calculated relative mouse position
+    console.log("Mouse Enter Position:", {
+      x: e.clientX - left,
+      y: e.clientY - top,
+    });
+  };
+
   return (
     <motion.div
       className="test-cards"
       key={index}
-      initial={{ y: 400, opacity: 0 }} // Set initial opacity
-      animate={isInView ? { y: 0, opacity: 1 } : { opacity: 0 }} // Animate opacity
+      initial={{ y: 400, opacity: 0 }}
+      animate={isInView ? { y: 0, opacity: 1 } : { opacity: 0 }}
       transition={{
         delay: index * 0.2,
         duration: 0.7,
       }}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+      //   onMouseMove={handleMouseEnter}
+      ref={cardRef}
     >
-      {item}
+      <div
+        className="circle-effect"
+        style={{
+          left: `${mousePos.x}%`,
+          top: `${mousePos.y}%`,
+          transform: `translate(-${mousePos.x}%, -${mousePos.y}%) scale(${circleSize})`,
+        }}
+      />
+      <div className="test-cards-inner">{item}</div>
     </motion.div>
   );
 }
