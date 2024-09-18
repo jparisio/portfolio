cced73ac39a3ad4923a2db43115afe64e39ad7d9
Justin Parisio <justinparisio@justins-m3-mbp.home>
2024-09-18 14:42:41 -0400

    working on projects

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -5,6 +5,7 @@ import {
   useTransform,
   useInView,
   resolveMotionValue,
+  color,
 } from "framer-motion";
 import { useEffect, useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
@@ -21,63 +22,78 @@ export default function Projects() {
   const isInView = useInView(ref, { once: false });
 
   const scale = useTransform(scrollYProgress, [0, 0.7], ["110%", "90%"]);
-  const scale2 = useTransform(scrollYProgress, [0, 0.7], ["110%", "130%"]);
+  const scale2 = useTransform(scrollYProgress, [0, 0.7], ["110%", "140%"]);
   const rotate = useTransform(scrollYProgress, [0, 0.7], [0, 90]);
-  const x = useTransform(scrollYProgress, [0.2, 0.7], [-200, 200]);
+  const x = useTransform(scrollYProgress, [0.2, 0.7], ["-65%", "50%"]);
+  const x2 = useTransform(scrollYProgress, [0.2, 0.7], ["-50vw", "15vw"]);
   const y = useTransform(scrollYProgress, [0.2, 0.7], [40, 0]);
+  const projY = useTransform(scrollYProgress, [0.7, 1], [0, -850]);
+  const borderRadius = useTransform(scrollYProgress, [0.7, 1], [0, 50]);
+  const opacity = useTransform(scrollYProgress, [0.3, 1], [0, 1]);
 
   console.log(x.get() >= 100);
 
-  const opacity = useTransform(
-    scrollYProgress,
-    [0, 0.3, 0.4, 1],
-    ["0%", "0%", "100%", "100%"]
-  );
+  // const opacity = useTransform(
+  //   scrollYProgress,
+  //   [0, 0.3, 0.4, 1],
+  //   ["0%", "0%", "100%", "100%"]
+  // );
 
   return (
-    <div className="projects-container" ref={container}>
-      <div className="inner">
-        <motion.div className="img-container" style={{ scale }}>
-          <motion.img
-            style={{ scale: scale2 }}
-            src="/src/assets/mountain.jpg"
-            alt="img"
-          ></motion.img>
-        </motion.div>
-      </div>
+    <>
+      <div className="projects-wrapper">
+        <div className="projects-container" ref={container}>
+          <div className="inner">
+            <motion.div className="img-container" style={{ scale }}>
+              <motion.img
+                style={{ scale: scale2 }}
+                src="/src/assets/mountain.jpg"
+                alt="img"
+              ></motion.img>
+            </motion.div>
+          </div>
 
-      <div className="inner-1">
-        <div style={{ display: "flex", alignItems: "center" }}>
-          {/* The "1" */}
-          <motion.h1 style={{ x, opacity, scale }} ref={ref}>
-            01
-          </motion.h1>
+          <div className="sliding-number">
+            <div style={{ display: "flex", alignItems: "center" }}>
+              {/* The "1" */}
+              <motion.h1
+                className="project-no"
+                style={{ x, scale, opacity: 1 }}
+                ref={ref}
+              >
+                01
+              </motion.h1>
 
-          {/* Vertical "PROJECT" */}
-          <div
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              marginLeft: "10px",
-            }}
-          >
-            {"PROJECT".split("").map((letter, index) => (
-              <motion.div
-                key={index}
-                className="project-rotated"
+              {/* Vertical "PROJECT" */}
+              <div
                 style={{
-                  x,
-                  opacity,
-                  rotate,
-                  y,
+                  display: "flex",
+                  flexDirection: "column",
+                  marginLeft: "10px",
                 }}
               >
-                {letter}
-              </motion.div>
-            ))}
+                {"PROJECT".split("").map((letter, index) => (
+                  <motion.div
+                    key={index}
+                    className="project-rotated"
+                    style={{
+                      x: x2,
+                      opacity,
+                    }}
+                  >
+                    {letter}
+                  </motion.div>
+                ))}
+              </div>
+            </div>
           </div>
         </div>
+        <motion.div
+          className="project-gallery"
+          style={{ y: projY }}
+        ></motion.div>
       </div>
-    </div>
+      <div className="project-gallery-one"></div>
+    </>
   );
 }
