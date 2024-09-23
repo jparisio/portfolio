0795b7bacb21109b69ebf3d5d2b7ff0e558f9c52
Justin Parisio <justinparisio@mac.home>
2024-09-23 15:41:40 -0400

    adding hover effect to projects

--- src/components/projects/Project.jsx
+++ src/components/projects/Project.jsx
@@ -1,27 +1,69 @@
 import React from "react";
-import { useRef } from "react";
-import { motion, useInView } from "framer-motion";
+import { useRef, useState } from "react";
+import { motion, useInView, useMotionValue, useSpring } from "framer-motion";
 import Magnetic from "../Magnetic";
 import "./Project.css";
 export default function Project() {
+  const [hovering, setHovering] = useState(false);
+  const [mousePos, setMousePos] = useState({
+    x: useMotionValue(0),
+    y: useMotionValue(0),
+  });
+  const scale = hovering ? 80 : 0;
   const ref = useRef(null);
   const isInView = useInView(ref, { once: true }); // Triggers animation only once
   const content =
     "A game maker project that I have been working on /for a little over a year now. Ive collaberated with people /across the world in order to bring this project to/ life. Includes high quality pixel art, fluid animations, /smooth gameplay and tight movement";
-  //   const container = useRef(null);
-  //   const { scrollYProgress } = useScroll({
-  //     target: container,
-  //     offset: ["start start ", "end end"],
-  //   });
 
-  //   const scale = useTransform(scrollYProgress, [0, 1], ["100%", "110%"]);
+  const handleMouseMove = (e) => {
+    const videoRect = e.target.getBoundingClientRect();
+    mousePos.x.set(e.clientX - videoRect.left - scale / 2);
+    mousePos.y.set(e.clientY - videoRect.top - scale / 2);
+  };
 
-  //   console.log(hovering);
+  const smoothOptions = {
+    stiffness: 200, // Higher stiffness for a snappier feel
+    damping: 12, // Lower damping for a bouncier effect
+    mass: 0.5, // A lighter mass for quicker reactions
+    bounce: 0.25, // Some bounce effect
+    restSpeed: 0.01, // Ensure the spring settles nicely
+    restDelta: 0.01, // Snaps to target smoothly};
+  };
+
+  const smoothMouse = {
+    x: useSpring(mousePos.x, smoothOptions),
+    y: useSpring(mousePos.y, smoothOptions),
+  };
+
+  // console.log(hovering);
+  // console.log(mousePos);
   return (
     <div className="project-container">
       <motion.div className="project-image">
-        <div className="image-content">
+        <motion.div
+          className="image-content"
+          onMouseEnter={() => setHovering(true)}
+          onMouseLeave={() => setHovering(false)}
+          onMouseMove={handleMouseMove}
+          onClick={() =>
+            window.open("https://www.youtube.com/watch?v=RECMi7wROhg", "_blank")
+          }
+        >
           <motion.img src="/portfolio-img3.jpg" alt="some pic" />
+          <motion.div
+            className="moving-clicker"
+            style={{
+              top: smoothMouse.y,
+              left: smoothMouse.x,
+              width: scale,
+              height: scale,
+            }}
+            animate={{ width: scale, height: scale }}
+            transition={{ duration: 0.3 }}
+          >
+            {hovering ? "VIEW" : ""}
+          </motion.div>
+
           <video src="/game.mp4" loop autoPlay muted playsinline></video>
           <div className="project-banner">
             <motion.div
@@ -59,7 +101,7 @@ export default function Project() {
               2024
             </motion.div>
           </div>
-        </div>
+        </motion.div>
       </motion.div>
 
       {/* <Magnetic optionalClass={"magnetic-hover"}>
