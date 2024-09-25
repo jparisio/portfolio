c3e82fd2f4c4f3307b22347268aacbb10fce1d13
Justin Parisio <justinparisio@mac.home>
2024-09-24 23:43:21 -0400

    somehow fixed a mobile issue

--- src/components/projects/Project.jsx
+++ src/components/projects/Project.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import { useRef, useState } from "react";
-import { motion, useInView, useMotionValue, useSpring } from "framer-motion";
+import {
+  frame,
+  motion,
+  useInView,
+  useMotionValue,
+  useSpring,
+} from "framer-motion";
 import Magnetic from "../Magnetic";
 import "./Project.css";
 export default function Project({
@@ -20,8 +26,6 @@ export default function Project({
   const scale = hovering ? 80 : 0;
   const ref = useRef(null);
   const isInView = useInView(ref, { once: true }); // Triggers animation only once
-  // const content =
-  //   "A game maker project that I have been working on /for a little over a year now. Ive collaberated with people /across the world in order to bring this project to/ life. Includes high quality pixel art, fluid animations, /smooth gameplay and tight movement";
 
   const handleMouseMove = (e) => {
     const videoRect = e.target.getBoundingClientRect();
@@ -40,8 +44,6 @@ export default function Project({
     y: useSpring(mousePos.y, smoothOptions),
   };
 
-  // console.log(hovering);
-  // console.log(mousePos);
   return (
     <div className="project-container">
       <motion.div className="project-image">
@@ -69,47 +71,23 @@ export default function Project({
 
           <video src={video} loop autoPlay muted playsinline></video>
           <div className="project-banner">
-            <motion.div
-              initial={{ opacity: 0 }}
-              animate={isInView ? { opacity: 100 } : {}}
-              transition={{
-                delay: 0.1,
-                duration: 1,
-                ease: [0.65, 0, 0.35, 1],
-              }}
-            >
-              {frameworks[0]}
-            </motion.div>
-            <motion.div
-              initial={{ opacity: 0 }}
-              animate={isInView ? { opacity: 100 } : {}}
-              transition={{
-                delay: 0.14,
-                duration: 1,
-                ease: [0.65, 0, 0.35, 1],
-              }}
-            >
-              {frameworks[1]}
-            </motion.div>
-            <motion.div
-              className="year"
-              initial={{ opacity: 0 }}
-              animate={isInView ? { opacity: 100 } : {}}
-              transition={{
-                delay: 0.18,
-                ease: [0.65, 0, 0.35, 1],
-                duration: 1,
-              }}
-            >
-              {frameworks[2]}
-            </motion.div>
+            {frameworks.map((framework, index) => (
+              <motion.div
+                className={index === frameworks.length - 1 ? "year" : ""}
+                initial={{ opacity: 0 }}
+                animate={isInView ? { opacity: 100 } : {}}
+                transition={{
+                  delay: 0.1,
+                  duration: 1,
+                  ease: [0.65, 0, 0.35, 1],
+                }}
+              >
+                {framework}
+              </motion.div>
+            ))}
           </div>
         </motion.div>
       </motion.div>
-
-      {/* <Magnetic optionalClass={"magnetic-hover"}>
-        <Magnetic optionalClass={"view"}>VIEW</Magnetic>
-      </Magnetic> */}
 
       <motion.div className="project-number">
         <h1>
