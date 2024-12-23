d9a18518d15a5e55de890d9466f3847db8ebc1ca
Justin Parisio <114594154+jparisio@users.noreply.github.com>
2024-12-22 22:05:45 -0500

    game preview change

--- src/App.jsx
+++ src/App.jsx
@@ -55,7 +55,7 @@ export default function App() {
       <About scroll={scrollYProgress} />
       <Projects />
       {/* </div> */}
-      <Scene />
+      {/* <Scene /> */}
       <Contact />
     </>
   );
